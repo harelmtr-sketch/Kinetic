dfd63f8c0dc374004a5285a7de0283e16b4befc5
harelmtr-sketch <harelmtr@gmail.com>
2026-03-08 18:47:13 +0200

    Fix RNGH root wiring for GestureDetector

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppShell from './src/navigation/AppShell';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AppShell />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AppShell />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
